4dadecc1feb4980c5fb3d5c2d4300802738a4c09
13772032410@163.com <13772032410@163.com>
2020-01-17 14:46:31 +0800

    1-17

--- myner/settings.py
+++ myner/settings.py
@@ -116,3 +116,14 @@ USE_TZ = True
 STATIC_URL = '/static/'
 
 MODEL_PATH = os.path.join(BASE_DIR, 'hitner/model/ltp_data_v3.4.0')
+
+REST_FRAMEWORK = {
+    'DEFAULT_PERMISSION_CLASSES': (
+        'rest_framework.permissions.IsAuthenticated',
+    ),
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework_jwt.authentication.JSONWebTokenAuthentication',
+        'rest_framework.authentication.SessionAuthentication',
+        'rest_framework.authentication.BasicAuthentication',
+    ),
+}
--- myner/urls.py
+++ myner/urls.py
@@ -15,9 +15,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from rest_framework_jwt.views import obtain_jwt_token
 from hitner import views as hitner_views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/nlp/', hitner_views.NerView.as_view()),
+    path(r'api-token-auth/', obtain_jwt_token),
 ]
